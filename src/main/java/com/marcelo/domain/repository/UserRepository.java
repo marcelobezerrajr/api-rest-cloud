5925eb48f459652d4e39d135b772e31544b7fd96
Marcelo Bezerra <marcelojuniorbzerra12@gmail.com>
2025-05-05 22:34:05 -0300

    Adicionado o UserRepository para usar o JPARepository e adicionado o application-dev.yml

---
+++ src/main/java/com/marcelo/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.marcelo.domain.repository;
+
+import com.marcelo.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
